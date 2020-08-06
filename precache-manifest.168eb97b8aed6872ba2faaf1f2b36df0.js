018c08d22dee4c92f216a859cdccccbe35fd6e1b
Leno <lunchboxno@gmail.com>
2020-08-07 00:22:11 +0900

    Updates

---
+++ precache-manifest.168eb97b8aed6872ba2faaf1f2b36df0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "293158c24fed49f3c45c9900f5b3d6e0",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "efa38c40412d5412b9ea",
+    "url": "/movie_app/static/css/main.09376af6.chunk.css"
+  },
+  {
+    "revision": "109356d05735fcb9e967",
+    "url": "/movie_app/static/js/2.670a5eaa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.670a5eaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efa38c40412d5412b9ea",
+    "url": "/movie_app/static/js/main.724ee5cb.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
